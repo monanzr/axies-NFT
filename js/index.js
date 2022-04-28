54ae92cc9400f5be7cf59d72e25c2aea55afb7ad
monanzr <monanazari8@hmail.com>
2022-04-29 01:56:12 +0430

    new edit 2

--- js/index.js
+++ js/index.js
@@ -39,3 +39,54 @@ document.querySelector(".light__mood").addEventListener("click", function () {
 document.querySelector(".dark__mood").addEventListener("click", function () {
     document.querySelector("html").classList.remove("light__theme");
 });
+
+
+function countDownDate(date, element) {
+    var now = new Date().getTime();
+    var distance = date - now;
+    
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+   element.innerHTML = days + " : " + hours + " : " + minutes + " : " + seconds;
+    
+  if (distance < 0) {
+    element.innerHTML = "EXPIRED";
+  } else {
+    setTimeout(countDownDate,1000);
+  }
+}
+
+const classNAme = document.getElementsByClassName("counter__text");
+for (let i = 0; i < classNAme.length; i++) {
+    countDownDate(
+        new Date("Jan 5, 2023 15:37:25").getTime(),
+        classNAme[i]
+    )
+    countDownDate(
+        new Date("April 5, 2023 15:37:25").getTime(),
+        classNAme[1]
+    )
+    countDownDate(
+        new Date("Jan 5, 2024 15:37:25").getTime(),
+        classNAme[3]
+    )
+}
+
+countDownDate(
+    new Date("Jan 5, 2023 15:37:25").getTime(),
+    document.querySelectorAll(".counter__text")
+)
+
+// countDownDate(
+//     new Date("May 5, 2023 15:37:25").getTime(),
+//     document.getElementById("counter__timer2")
+// )
+
+// countDownDate(
+//     new Date("April 5, 2023 15:37:25").getTime(),
+//     document.getElementById("counter__timer5")
+// )
